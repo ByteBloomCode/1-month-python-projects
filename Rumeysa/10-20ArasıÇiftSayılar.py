42ae544de3debc6656205ba47a83b5c73266a5a7
viormys <rumeysaozdogann16@gmail.com>
2025-11-11 16:09:42 +0300

    Add files via upload

---
+++ Rumeysa/10-20ArasıÇiftSayılar.py
@@ -0,0 +1,5 @@
+#for döngüsü kullanarak 10-20 arası çift sayılar 
+for sayi in range(10,21):
+    if sayi%2==0:
+        print(sayi)
+    
---
+++ Rumeysa/30-40ArasıTekSayılar.py
@@ -0,0 +1,6 @@
+#while ve sayaç kullanarak 1-40 arası tek sayılar
+sayac=1
+while sayac<=40:
+    if sayac%2==1:
+        print(sayac)
+    sayac=sayac+1

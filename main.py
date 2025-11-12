c865c4f8cfc00eff84ed37ad25074c60abbae43c
jallo-dev <jallownoor@gmail.com>
2025-11-12 14:14:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#adjective1 = input("Enter an adjective: ")
+#noun = input("Enter a noun: ")
+#adjective2 = input("Enter an adjective: ")
+#verb = input("Enter a verb: ")
+#adjective3 = input("Enter an adjective: ")
+
+#print(f"Today I went to {adjective1} zoo")
+#print(f"In an exibite, I saw {noun}" )
+#print(f"{noun} was {adjective2} and {verb}")
+#print(f"I was {adjective3}")
+
+
+#item = input("What would you like to buy?: ")
+#price = float(input("The price is: "))
+#quantity = int(input("How many do you want?:"))
+
+#total = quantity * price
+
+#print(f"If you bought {quantity} x {item}/s")
+#print(f"Your total is: ${total}")
+
